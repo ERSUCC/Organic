32f016aaba11543907e2c62c793f6cafb840dc9f
ERSUCC <isaacbrown5763@gmail.com>
2026-08-03 00:45:01 -0500

    Add release scripts for Mac

---
+++ dist/mac/package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+cmake -B build
+cmake --build build --config Release
+cmake --install build --config Release --prefix install
+
+INSTALL_ROOT=/usr/local
+
+install_name_tool -add_rpath $INSTALL_ROOT/lib/organic install/bin/organic
+
+TMP_ROOT=/tmp/organic_dist
+
+rm -rf $TMP_ROOT
+mkdir -p $TMP_ROOT/bin $TMP_ROOT/lib/organic
+
+cp install/bin/organic $TMP_ROOT/bin
+cp install/lib/*.dylib $TMP_ROOT/lib/organic
+
+pkgbuild --root $TMP_ROOT --identifier Organic --version 0.1.0 --install-location $INSTALL_ROOT $TMP_ROOT/OrganicExec.pkg
+productbuild --distribution dist/mac/distribution.xml --package-path $TMP_ROOT install/Organic.pkg
