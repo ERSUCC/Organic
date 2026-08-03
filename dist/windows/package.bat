52a0fd38495f820f31c9822f4077f9cf90e894c0
ERSUCC <isaacbrown5763@gmail.com>
2026-08-02 23:26:19 -0500

    Add release scripts for Windows

---
+++ dist/windows/package.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cmake -B build || exit /b 1
+cmake --build build --config Release || exit /b 1
+cmake --install build --config Release --prefix install || exit /b 1
+
+for /f "tokens=* USEBACKQ" %%x in (`powershell -Command "[guid]::NewGuid()"`) do (
+  set guid=%%x
+)
+
+wix build -src dist\windows\Organic.wxs -arch x64 -pdbtype none -bindpath install -out install\Organic.msi ^
+          -define name=Organic -define version=0.1.0 -define guid=%guid% -define root="%cd%\install" || exit /b 1
