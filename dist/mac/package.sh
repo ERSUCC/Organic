b040287f999ba848c65ec301a1507d80410c6525
ERSUCC <isaacbrown5763@gmail.com>
2026-08-25 00:46:14 -0500

    Fix dependency resolution in Mac packaging

--- dist/mac/package.sh
+++ dist/mac/package.sh
@@ -8,19 +8,30 @@ cmake -B build -DORGANIC_VERSION=$VERSION
 cmake --build build --config Release
 cmake --install build --config Release --prefix install
 
-INSTALL_ROOT=/usr/local
+./build/unify install/bin/organic install/lib
 
-install_name_tool -add_rpath $INSTALL_ROOT/lib/organic install/bin/organic
+install_name_tool -add_rpath @executable_path/../lib install/bin/organic
 
+codesign -s - -f install/lib/*.dylib
+
+INSTALL_ROOT=/opt/organic
 TMP_ROOT=/tmp/organic-$VERSION
 
 rm -rf $TMP_ROOT
-mkdir -p $TMP_ROOT/bin $TMP_ROOT/lib/organic
+mkdir -p $TMP_ROOT/bin $TMP_ROOT/lib
 
 cp install/bin/organic $TMP_ROOT/bin
-cp install/lib/*.dylib $TMP_ROOT/lib/organic
+cp install/lib/*.dylib $TMP_ROOT/lib
 
-pkgbuild --root $TMP_ROOT --identifier Organic --version $VERSION --install-location $INSTALL_ROOT "$TMP_ROOT/OrganicExec.pkg"
-productbuild --distribution dist/mac/distribution.xml --package-path $TMP_ROOT "install/Organic $VERSION.pkg"
+pkgbuild --root $TMP_ROOT --identifier OrganicExec --version $VERSION --install-location $INSTALL_ROOT /tmp/OrganicExec.pkg
+
+rm -rf $TMP_ROOT
+mkdir -p $TMP_ROOT/paths.d
+
+echo $INSTALL_ROOT/bin > $TMP_ROOT/paths.d/organic
+
+pkgbuild --root $TMP_ROOT --identifier OrganicPath --version $VERSION --install-location /etc /tmp/OrganicPath.pkg
+
+productbuild --distribution dist/mac/distribution.xml --package-path /tmp "install/Organic $VERSION.pkg"
 
 rm -rf $TMP_ROOT
