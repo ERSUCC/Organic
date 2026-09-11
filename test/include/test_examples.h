6a063012f6a8698f81ae9b8b189b4f7ab879afea
ERSUCC <isaacbrown5763@gmail.com>
2026-09-10 21:40:39 -0500

    Refactor and add more examples

--- test/include/test_examples.h
+++ test/include/test_examples.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <stddef.h>
+
 #include "exception.h"
 #include "parse.h"
 #include "path.h"
+#include "program.h"
 #include "resolve.h"
 #include "source.h"
 #include "test.h"
 #include "test_utils.h"
 #include "token.h"
 #include "transform.h"
+#include "utils.h"
 
 struct TestExamples : public Test
 {
@@ -21,5 +25,7 @@ private:
     TestExamples(TestTracker* tracker);
 
     void expectSuccess(const Path& path);
+
+    Utils* utils;
 
 };
